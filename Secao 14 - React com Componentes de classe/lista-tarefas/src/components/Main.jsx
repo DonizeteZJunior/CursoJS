ad9e2170f590858d313171c4efae0e1d48dba180
Donizete A Z Junior <donizete.dev@gmail.com>
2025-07-04 16:51:45 -0300

    Estilizando o Form e já deixando a lista alinhada.

--- Secao 14 - React com Componentes de classe/lista-tarefas/src/components/Main.jsx
+++ Secao 14 - React com Componentes de classe/lista-tarefas/src/components/Main.jsx
@@ -1,10 +1,21 @@
 import React, { Component } from "react";
 
+// Form
+import { FaPlus } from 'react-icons/fa'
+
+// Tarefas
+import { FaEdit, FaWindowClose } from "react-icons/fa";
+
 import './Main.css'
 
 export default class Main extends Component{
   state = {
     novaTarefa: '',
+    tarefas: [
+      'Fazer café',
+      'Beber água',
+      'Estudar',
+    ]
   };
 
   handleChange = (e) => {
@@ -14,16 +25,30 @@ export default class Main extends Component{
   }
 
   render(){
-    // const { novaTarefa } = this.state;
+    const { novaTarefa, tarefas } = this.state;
 
     return (
       <div className="main">
         <h1>Lista de tarefas</h1>
 
-        <form action="#">
-          <input type="text" onChange={this.handleChange} />
-          <button type="submit">Enviar</button>
+        <form action="#" className="form">
+          <input type="text" onChange={this.handleChange} value={novaTarefa} />
+          <button type="submit">
+            <FaPlus />
+          </button>
         </form>
+
+        <ul className="tarefas">
+          {tarefas.map(tarefa => (
+            <li key={tarefa}>
+              {tarefa}
+              <div>
+                <FaEdit className="edit" />
+                <FaWindowClose className="delete" />
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
