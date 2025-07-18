bbaa650319db5282399e81db401637ae1437d084
Donizete A Z Junior <donizete.dev@gmail.com>
2025-07-18 15:40:57 -0300

    criando as páginas e rotas

--- Secao 15 - React - Redux - Hooks/src/routes/index.js
+++ Secao 15 - React - Redux - Hooks/src/routes/index.js
@@ -3,13 +3,22 @@ import { Switch } from "react-router-dom";
 // import { toast } from "react-toastify";
 
 import MyRoute from "./MyRoute";
+import Aluno from '../pages/Aluno';
+import Alunos from '../pages/Alunos';
+import Fotos from '../pages/Fotos';
 import Login from '../pages/Login';
+import Registro from '../pages/Registro';
 import Pagina404 from '../pages/Pagina404';
 
 export default function Routes(){
   return (
     <Switch>
-      <MyRoute exact path="/" component={Login} isClosed={false} />
+      <MyRoute exact path="/" component={Alunos} isClosed={false} />
+      <MyRoute exact path="/aluno/:id/edit" component={Aluno} isClosed />
+      <MyRoute exact path="/aluno/" component={Aluno} isClosed />
+      <MyRoute exact path="/fotos/:id" component={Fotos} isClosed />
+      <MyRoute exact path="/login/" component={Login} isClosed={false} />
+      <MyRoute exact path="/registro/" component={Registro} isClosed={false} />
       <MyRoute path="*" component={Pagina404} />
     </Switch>
   );
