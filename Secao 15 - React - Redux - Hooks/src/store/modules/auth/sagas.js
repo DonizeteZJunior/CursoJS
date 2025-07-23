5c897eb369dc60c6fd8dad3af259468486828762
Donizete A Z Junior <donizete.dev@gmail.com>
2025-07-23 17:15:16 -0300

    Edição de usuário logado.

--- Secao 15 - React - Redux - Hooks/src/store/modules/auth/sagas.js
+++ Secao 15 - React - Redux - Hooks/src/store/modules/auth/sagas.js
@@ -16,6 +16,7 @@ function* loginRequest({payload}){
     historico.push(payload.prevPath);
   } catch(e) {
     toast.error('Usuário ou senha inválidos.');
+    console.log(e);
     yield put(actions.loginFailure());
   }
 }
@@ -26,7 +27,50 @@ function persistRehydrate({ payload }){
   axios.defaults.headers.Authorization = `Bearer ${token}`;
 }
 
+function* registerRequest({payload}){
+  const { id, nome, email, password } = payload;
+
+  try{
+    if(id) {
+      yield call(axios.put, '/users', {
+        email,
+        nome,
+        password: password || undefined,
+      });
+      toast.success('Conta alterada com sucesso!');
+      yield put(actions.registerUpdatedSuccess({nome, email, password}));
+    } else {
+      yield call(axios.post, '/users', {
+        email,
+        nome,
+        password,
+      });
+      toast.success('Conta criada com sucesso!');
+      yield put(actions.registerCreatedSuccess({nome, email, password}));
+      historico.push('/login');
+    }
+  } catch(e) {
+    const errors = get(e, 'response.data.error', []);
+    const status = get(e, 'response.status', 0);
+
+    if ( status === 401){
+      toast.error('Você precisa fazer login novamente.');
+      yield put(actions.loginFailure());
+      return historico.push('/login');
+    }
+
+    if(errors.length > 0){
+      errors.map(error => toast.error(error));
+    } else {
+      toast.error('Erro desconhecido');
+    }
+
+    yield put(actions.registerFailure());
+  }
+}
+
 export default all([
   takeLatest(types.LOGIN_REQUEST, loginRequest),
-  takeLatest(types.PERSIST_REHYDRATE, persistRehydrate)
+  takeLatest(types.PERSIST_REHYDRATE, persistRehydrate),
+  takeLatest(types.REGISTER_REQUEST, registerRequest),
 ]);
