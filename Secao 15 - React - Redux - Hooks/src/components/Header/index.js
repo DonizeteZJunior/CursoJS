c6108e9cfa08c807df5bd80f82b05ff337630d6d
Donizete A Z Junior <donizete.dev@gmail.com>
2025-07-24 16:19:16 -0300

    Opção de logout e excluir aluno.

--- Secao 15 - React - Redux - Hooks/src/components/Header/index.js
+++ Secao 15 - React - Redux - Hooks/src/components/Header/index.js
@@ -1,16 +1,41 @@
 import React from "react";
-import { FaHome, FaSignInAlt, FaUserAlt } from 'react-icons/fa';
+import { FaHome, FaSignInAlt, FaUserAlt, FaCircle, FaPowerOff } from 'react-icons/fa';
 import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
 
+import * as actions from '../../store/modules/auth/actions';
+import historico from '../../services/historico';
 import { Nav } from "./styled";
 
 export default function Header(){
+  const dispatch = useDispatch();
+  const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
+
+  const handleLogout = e => {
+    e.preventDefault();
+    dispatch(actions.loginFailure());
+    historico.push('/');
+  };
 
   return (
     <Nav>
-      <Link to="/"><FaHome size={24}/></Link>
-      <Link to="/registro"><FaUserAlt size={24}/></Link>
-      <Link to="/login"><FaSignInAlt size={24}/></Link>
+      <Link to="/">
+        <FaHome size={24}/>
+      </Link>
+      <Link to="/registro">
+        <FaUserAlt size={24}/>
+      </Link>
+      {isLoggedIn ? (
+        <Link onClick={handleLogout} to="/login">
+          <FaPowerOff size={24}/>
+        </Link>
+      ) : (
+        <Link to="/login">
+          <FaSignInAlt size={24}/>
+        </Link>
+      )}
+
+      {isLoggedIn && <FaCircle size={24} color="#66ff33" />}
     </Nav>
   );
 }
